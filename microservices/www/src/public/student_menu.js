b07ab45e4751ed1e1f52118731c12e0fd5c2be7c
santhosh kumar <santhoshkumar.ssk3222@gmail.com>
2018-01-09 01:58:35 +0530

    solution can be read

--- microservices/www/src/public/student_menu.js
+++ microservices/www/src/public/student_menu.js
@@ -407,6 +407,24 @@ function Solutionnotfavourable(Name,STAGE){
                                   if(STAGE<=2) 
                                     STAGE+=+1;
                                   STAGE=STAGE.toString();
+                                  $.ajax({
+                                        url: "https://notify.bulimic45.hasura-app.io/v1/send/email",
+                                        contentType: "application/json",
+                                        headers: {
+                                            "Authorization": "Bearer 4af1623b3c51f78e03754e69c60d3490f5509de98a7cc57a"
+                                        },
+                                        data: JSON.stringify({
+                                            "to": profile_row["email"],
+                                            "from": "clggrievances@gmail.com",
+                                            "fromName": "SVCE Grievance Redressal Committee",
+                                            "sub": "successfully  post of your grievance",
+                                            "text": "Dear "+profile_row['fname']+' '+profile_row['mname']+' '+profile_row['lname']+",\nGreetings from SVCE Grievance Redressal System.!! You have successfully posted your Problem to Higher level Grievance Redressal Committee and \nYou will soon recieve the solution for your problem from our commitee Regards,SVCE Grievance Team",
+                                            "html": "Dear "+profile_row['fname']+' '+profile_row['mname']+' '+profile_row['lname']+",<br><br>Greetings from SVCE Grievance Redressal System.!!<br>You have successfully posted your Problem to Higher level Grievance Redressal Committee and <br>You will soon recieve the solution for your problem from our commitee<br><br>Regards,<br>SVCE Grievance Team<br>"
+                                        }),
+                                        type: "POST",
+                                        dataType: "json"
+                                      });
+
                                 $.ajax({
                                   url: "https://data.bulimic45.hasura-app.io/v1/query",
                                   contentType: "application/json",
@@ -428,27 +446,17 @@ function Solutionnotfavourable(Name,STAGE){
                                   }),
                                   type: "POST",
                                   dataType: "json"
-                                });
-                                  $.ajax({
-                                        url: "https://notify.bulimic45.hasura-app.io/v1/send/email",
-                                        contentType: "application/json",
-                                        headers: {
-                                            "Authorization": "Bearer 4af1623b3c51f78e03754e69c60d3490f5509de98a7cc57a"
-                                        },
-                                        data: JSON.stringify({
-                                            "to": profile_row["email"],
-                                            "from": "clggrievances@gmail.com",
-                                            "fromName": "SVCE Grievance Redressal Committee",
-                                            "sub": "successfully  post of your grievance",
-                                            "text": "Dear "+profile_row['fname']+' '+profile_row['mname']+' '+profile_row['lname']+",\nGreetings from SVCE Grievance Redressal System.!! You have successfully posted your Problem to Higher level Grievance Redressal Committee and \nYou will soon recieve the solution for your problem from our commitee Regards,SVCE Grievance Team",
-                                            "html": "Dear "+profile_row['fname']+' '+profile_row['mname']+' '+profile_row['lname']+",<br><br>Greetings from SVCE Grievance Redressal System.!!<br>You have successfully posted your Problem to Higher level Grievance Redressal Committee and <br>You will soon recieve the solution for your problem from our commitee<br><br>Regards,<br>SVCE Grievance Team<br>"
-                                        }),
-                                        type: "POST",
-                                        dataType: "json"
-                                      });
+                                }).done(function(json) {
                                 alert("Thankyou for using our portal your problem as been poster to higher level");
                                 window.open("./student_menu.html","_self");
-                              }
+                              
+                                }).fail(function(xhr, status, errorThrown) {
+                                    console.log("Error: " + errorThrown);
+                                    console.log("Status: " + status);
+                                    console.dir(xhr);
+                                  });
+                                  
+                                }
                               else {
                                 $.ajax({
                                   url: "https://data.bulimic45.hasura-app.io/v1/query",
